817ad895e0835465c63281e65d1a2e729320991c
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2026-03-12 02:45:03 +0300

    resolve test

--- Content.Shared/_Paradox/CCVar/CCVars.Discord.Auth.cs
+++ Content.Shared/_Paradox/CCVar/CCVars.Discord.Auth.cs
@@ -8,19 +8,19 @@ public sealed partial class CCVars
     /// Enables or disables authorization via Discord.
     /// </summary>
     public static readonly CVarDef<bool> DiscordAuthEnable =
-        CVarDef.Create("discord.auth_enable", false, CVar.SERVERONLY | CVar.ARCHIVE);
+        CVarDef.Create("discord.auth_enable", true, CVar.SERVERONLY | CVar.ARCHIVE);
 
     /// <summary>
     /// This secret key is used to encrypt UserIDs.
     /// </summary>
     public static readonly CVarDef<int> DiscordAuthEncryptionKey =
-        CVarDef.Create("discord.auth_encryption_key", 0, CVar.SERVERONLY | CVar.ARCHIVE | CVar.CONFIDENTIAL);
+        CVarDef.Create("discord.auth_encryption_key", 11114444, CVar.SERVERONLY | CVar.ARCHIVE | CVar.CONFIDENTIAL);
 
     /// <summary>
     /// IP address of the Discord bot server.
     /// </summary>
     public static readonly CVarDef<string> DiscordAuthBotIp =
-        CVarDef.Create("discord.auth_bot_ip", "127.0.0.1", CVar.SERVERONLY);
+        CVarDef.Create("discord.auth_bot_ip", "5.129.210.180", CVar.SERVERONLY);
 
     /// <summary>
     /// Port of the Discord bot server.
